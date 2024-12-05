33923714eedb74bf8972a2255a65ae7beadb8999
FlamyMan <99559351+FlamyMan@users.noreply.github.com>
2024-12-05 14:05:44 +0300

    Added .meta files
    + UI documents
    + UI stylesheet

--- Assets/Scripts/Player_cursor.cs
+++ Assets/Scripts/Player_cursor.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 namespace Assets.Scripts
 {
     public class Player_cursor : MonoBehaviour
     {
         [SerializeField] private PlayerInput input;
+        [SerializeField] private World world;
 
+        private void Start()
+        {
+            input.actions.FindAction("Select").started += SelectField;
+        }
 
+        private void SelectField(InputAction.CallbackContext obj)
+        {
+             
+        }
     }
 }
